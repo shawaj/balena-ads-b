6c6a0833e574210949d0875a3cf417d4a2a0d09b
Aaron Shaw <shawaj@gmail.com>
2023-11-20 23:19:42 +0000

    Create start.sh

---
+++ wingbits/start.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e
+
+# Check if service has been disabled through the DISABLED_SERVICES environment variable.
+
+if [[ ",$(echo -e "${DISABLED_SERVICES}" | tr -d '[:space:]')," = *",$BALENA_SERVICE_NAME,"* ]]; then
+        echo "$BALENA_SERVICE_NAME is manually disabled."
+        sleep infinity
+fi
+
+# Verify that all the required varibles are set before starting up the application.
+
+echo "Verifying settings..."
+echo " "
+sleep 2
+
+missing_variables=false
+        
+# Begin defining all the required configuration variables.
+
+[ -z "$WINGBITS_DEVICE_ID" ] && echo "Plane Finder Sharecode is missing, will abort startup." && missing_variables=true || echo "Plane Finder Sharecode is set: $WINGBITS_DEVICE_ID"
+[ -z "$RECEIVER_HOST" ] && echo "Receiver host is missing, will abort startup." && missing_variables=true || echo "Receiver host is set: $RECEIVER_HOST"
+[ -z "$RECEIVER_PORT" ] && echo "Receiver port is missing, will abort startup." && missing_variables=true || echo "Receiver port is set: $RECEIVER_PORT"
+
+# End defining all the required configuration variables.
+
+echo " "
+
+if [ "$missing_variables" = true ]
+then
+        echo "Settings missing, aborting..."
+        echo " "
+        sleep infinity
+fi
+
+echo "Settings verified, proceeding with startup."
+echo " "
+
+# Variables are verified – continue with startup procedure.
+
+# Configure Wingbits/Vector according to environment variables.
+ "echo \"DEVICE_ID=\\\"${WINGBITS_DEVICE_ID}\\\"\" | sudo tee -a /etc/default/vector > /dev/null"
+
+# Start vector and put it in the background.
+/usr/bin/vector --watch-config &
+
+# Wait for any services to exit.
+wait -n
