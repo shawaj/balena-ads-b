7487d1ea1ac4dfc4a1cb375485b59381443eb02c
Aaron Shaw <shawaj@gmail.com>
2023-11-21 19:49:08 +0000

    Update defli-installer.sh

--- defli/defli-installer.sh
+++ defli/defli-installer.sh
@@ -1,1 +1,10 @@
-defli install
+#!/usr/bin/env bash
+set -e
+
+arch="$(dpkg --print-architecture)"
+echo System Architecture: $arch
+
+cd /tmp
+
+git clone https://github.com/dbsoft42/adsb-data-collector-mongodb.git
+pip3 install aiohttp motor pymongo python-dateutil dnspython
