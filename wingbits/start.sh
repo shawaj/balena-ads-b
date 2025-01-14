b0ccaa54441f6eccbc82da58934d988f6f841a93
Aaron Shaw <shawaj@gmail.com>
2025-01-14 01:39:08 +0000

    feat: Wingbits cli prep (#183)
    Prepare for wingbits-cli with geosigner dongle

--- wingbits/start.sh
+++ wingbits/start.sh
@@ -38,18 +38,59 @@ fi
 echo "Settings verified, proceeding with startup."
 echo " "
 
-# Check if Wingbits is latest version
+# Check if Wingbits is latest version and update if not
 
-local_version=$(cat /etc/wingbits/version)
+# Determine the architecture
+GOOS="linux"
+case "$(uname -m)" in
+	x86_64)
+		GOARCH="amd64"
+		;;
+	i386|i686)
+		GOARCH="386"
+		;;
+	armv7l)
+		GOARCH="arm"
+		;;
+	aarch64|arm64)
+		GOARCH="arm64"
+		;;
+	*)
+		echo "Unsupported architecture"
+  		exit 1
+		;;
+esac
+
+WINGBITS_PATH="/etc/wingbits"
+local_version=$(cat $WINGBITS_PATH/version)
+local_json_version=$(cat $WINGBITS_PATH/json-version)
 echo "Current local version: $local_version"
+echo "Current local build: $local_json_version"
 
-SCRIPT_URL="https://gitlab.com/wingbits/config/-/raw/master/download.sh"
-script=$(curl $SCRIPT_URL)
+SCRIPT_URL="https://install.wingbits.com/download.sh"
+JSON_URL="https://install.wingbits.com/$GOOS-$GOARCH.json"
+script=$(curl -s $SCRIPT_URL)
 version=$(echo "$script" | grep -oP '(?<=WINGBITS_CONFIG_VERSION=")[^"]*')
-echo "Latest available wingbits version: $version"
+script_json=$(curl -s $JSON_URL)
+json_version=$(echo "$script_json" | jq -r '.Version')
 
-if [ "$version" != "$local_version" ] || [ -z "$version" ]; then
-    echo "WARNING: You are not running the latest Wingbits version. Please update at your earliest convenience."
+echo "Latest available Wingbits version: $version"
+echo "Latest available Wingbits build: $json_version"
+
+if [ "$version" != "$local_version" ] || [ "$json_version" != "$local_json_version" ] || [ -z "$json_version" ] || [ -z "$version" ]; then
+    echo "WARNING: You are not running the latest Wingbits version. Updating..."
+    echo "Architecture: $GOOS-$GOARCH"
+    rm -rf $WINGBITS_PATH/wingbits.gz
+    curl -s -o $WINGBITS_PATH/wingbits.gz "https://install.wingbits.com/$json_version/$GOOS-$GOARCH.gz"
+    rm -rf $WINGBITS_PATH/wingbits
+    gunzip $WINGBITS_PATH/wingbits.gz 
+    chmod +x $WINGBITS_PATH/wingbits
+    rm -rf $WINGBITS_PATH/config.json
+    curl -s -o $WINGBITS_PATH/config.json "https://install.wingbits.com/config.json"
+    echo "$version" > $WINGBITS_PATH/version
+    echo "$json_version" > $WINGBITS_PATH/json-version
+    echo "New Wingbits version installed: $version"
+    echo "New Wingbits build installed: $json_version"
 else
     echo "Wingbits is up to date"
 fi
@@ -58,9 +99,17 @@ echo " "
 
 # Variables are verified – continue with startup procedure.
 
-# Start vector and readsb and put in the background.
-/usr/bin/vector --watch-config &
+# Place correct station ID in config.json and /etc/wingbits/device
+station="$(jq --arg a "$WINGBITS_DEVICE_ID" '.station = $a' $WINGBITS_PATH/config.json)"
+echo -E "${station}" > $WINGBITS_PATH/config.json
+echo -E "${WINGBITS_DEVICE_ID}" > $WINGBITS_PATH/device
+
+# Move to wingbits folder
+cd $WINGBITS_PATH
+
+# Start readsb and wingbits feeder and put in the background.
 /usr/bin/feed-wingbits --net --net-only --debug=n --quiet --net-connector localhost,30006,json_out --write-json /run/wingbits-feed --net-beast-reduce-interval 0.5 --net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30154 --net-bo-port 0 --net-ri-port 0 --net-connector "$RECEIVER_HOST","$RECEIVER_PORT",beast_in 2>&1 | stdbuf -o0 sed --unbuffered '/^$/d' |  awk -W interactive '{print "[readsb-wingbits]     " $0}' &
+./wingbits feeder start 2>&1 | stdbuf -o0 sed --unbuffered '/^$/d' |  awk -W interactive '{print "[wingbits-feeder]     " $0}' &
 
 # Wait for any services to exit.
 wait -n
--- wingbits/wingbits_installer.sh
+++ wingbits/wingbits_installer.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
-arch="$(dpkg --print-architecture)"
-echo System Architecture: $arch
+function setup_wingbits_client() {
+	# Determine the architecture
+        GOOS="linux"
+	case "$(uname -m)" in
+		x86_64)
+			GOARCH="amd64"
+			;;
+		i386|i686)
+			GOARCH="386"
+			;;
+		armv7l)
+			GOARCH="arm"
+			;;
+		aarch64|arm64)
+			GOARCH="arm64"
+			;;
+		*)
+			echo "Unsupported architecture"
+			exit 1
+			;;
+	esac
+	WINGBITS_PATH="/etc/wingbits"
+	echo "Architecture: $GOOS-$GOARCH"
+	mkdir -p $WINGBITS_PATH
+	curl -o $WINGBITS_PATH/wingbits.gz "https://install.wingbits.com/$WINGBITS_COMMIT_ID/$GOOS-$GOARCH.gz"
+	gunzip $WINGBITS_PATH/wingbits.gz 
+	chmod +x $WINGBITS_PATH/wingbits
+	curl -o $WINGBITS_PATH/config.json "https://install.wingbits.com/config.json"
+}
 
-cd /tmp
-
-bash -c "$(curl -L https://setup.vector.dev)"
-apt-get -y install vector
+setup_wingbits_client
